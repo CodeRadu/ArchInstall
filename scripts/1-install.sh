614cad9de98784b5a2e50a3c4b6f450c1c5f73f9
CodeRadu <radu.gabi.m@gmail.com>
2023-01-24 12:53:49 +0200

    fix grub config

--- scripts/1-install.sh
+++ scripts/1-install.sh
@@ -11,9 +11,10 @@ pacstrap /mnt --noconfirm grub efibootmgr sed
 
 arch-chroot /mnt /bin/bash <<"EOS"
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
-sed -i 's/quiet splash//' /etc/default/grub
+sed -i 's/loglevel=3 quiet//' /etc/default/grub
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
-sed -i 's/menu/hidden/' /etc/default/grub
+sed -i 's/GRUB_TIMEOUT_STYLE=menu/GRUB_TIMEOUT_STYLE=hidden/' /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
 EOS
 
 echo "Done installing base system"
