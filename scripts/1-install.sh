5a1f26aea82ead4aa2eb990e88a0a99ba6d90ccc
CodeRadu <radu.gabi.m@gmail.com>
2023-01-24 11:44:27 +0200

    move grub install

--- scripts/1-install.sh
+++ scripts/1-install.sh
@@ -3,12 +3,13 @@
 disk="/dev/$1"
 
 echo "---------------Installing base----------------"
-pacstrap /mnt base linux linux-firmware nano sudo grub efibootmgr --noconfirm
+pacstrap /mnt base linux linux-firmware --noconfirm
 genfstab -L /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt
 
 echo "---------------Installing grub----------------"
+pacstrap /mnt grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/mnt/boot --bootloader-id=grub
 sed 's/quiet splash//' /mnt/etc/default/grub
 sed 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /mnt/etc/default/grub
