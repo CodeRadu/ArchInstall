5d20509a6e61088d6f664cc111384ca02c6c8e8e
CodeRadu <radu.gabi.m@gmail.com>
2023-01-24 11:15:09 +0200

    Setup script

---
+++ archinstall.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/bash
+
+function quit() {
+  reason=$1
+  echo "Installation failed in '$reason' script"
+  exit 1
+}
+
+echo -ne "
+----------Radu's arch install script----------
+This script automatically installs arch linux to your computer
+----------------------------------------------
+WARNING: If you're not using UEFI, your system will not boot!
+"
+lsblk -d
+echo "Choose disk (sda, sdb..)"
+read diskname
+if [ ! -b "/dev/$diskname" ]; then
+  echo "Not a block device"
+  exit 1
+fi
+echo "Running setup script"
+bash scripts/0-setup.sh $diskname || quit "setup"
+# echo "Running install script"
---
+++ scripts/0-setup.sh
@@ -0,0 +1,37 @@
+#! /usr/bin/bash
+
+disk="/dev/$1"
+
+# Install keyring
+echo "---------Installing required packages---------"
+pacman -Sy --noconfirm archlinux-keyring gptfdisk
+
+# Format disk
+echo "--------------Partitioning disk---------------"
+umount -A --recursive /mnt
+
+# Zap the disk
+sgdisk -Z $disk # Clear the disk
+sgdisk -o $disk # Make gpt structure
+
+# Create the partitions
+sgdisk -n 1::+300M --typecode=1:ef00 --change-name=1:'EFI boot' $disk # partition 1 EFI boot
+sgdisk -n 2::-1024M --typecode=2:8300 --change-name=2:'Root' $disk # partition 2 rootfs
+sgdisk -n 3::-0 --typecode=3:8200 --change-name=3:'Swap' $disk # partition 3 swap
+
+boot="${disk}1"
+root="${disk}2"
+swap="${disk}3"
+
+echo "---------------Formatting disk----------------"
+mkfs -t fat -F 32 $boot
+mkfs -t ext4 $root
+mkswap $swap
+
+echo "----------------Mounting disk-----------------"
+mount $root /mnt
+mkdir /mnt/boot
+mount $boot /mnt/boot
+swapon $swap
+
+echo "Setup script done!"
