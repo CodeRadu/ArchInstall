0db746c867b1822f26ad3101c0765e69b64aa267
CodeRadu <radu.gabi.m@gmail.com>
2023-01-24 11:28:00 +0200

    Add nvme disks

--- scripts/0-setup.sh
+++ scripts/0-setup.sh
@@ -19,9 +19,15 @@ sgdisk -n 1::+300M --typecode=1:ef00 --change-name=1:'EFI boot' $disk # partitio
 sgdisk -n 2::-1024M --typecode=2:8300 --change-name=2:'Root' $disk # partition 2 rootfs
 sgdisk -n 3::-0 --typecode=3:8200 --change-name=3:'Swap' $disk # partition 3 swap
 
-boot="${disk}1"
-root="${disk}2"
-swap="${disk}3"
+if [[ "${disk}" =~ "nvme" ]]; then
+  boot="${disk}p1"
+  root="${disk}p2"
+  swap="${disk}p3"
+else
+  boot="${disk}1"
+  root="${disk}2"
+  swap="${disk}3"
+fi
 
 echo "---------------Formatting disk----------------"
 mkfs -t fat -F 32 $boot
