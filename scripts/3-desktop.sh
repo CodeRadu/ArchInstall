f3083fcf664dd4e4fdac45a59aedb5322c191c86
CodeRadu <radu.gabi.m@gmail.com>
2023-01-25 13:34:07 +0200

    add desktop env

--- scripts/3-desktop.sh
+++ scripts/3-desktop.sh
@@ -1,11 +1,39 @@
 #! /usr/bin/bash
 
-echo "----------------Installing DE-----------------"
-arch-chroot /mnt /bin/bash << EOS
-pacman -S --noconfirm plasma
-pacman -S --noconfirm chromium kitty packagekit-qt5 flatpak
+function install_gnome() {
+  arch-chroot /mnt /bin/bash << EOS
+pacman -S --noconfirm gnome
+systemctl enable gdm
+EOS
+}
 
+function install_kde() {
+  arch-chroot /mnt /bin/bash << EOS
+pacman -S --noconfirm plasma
 systemctl enable sddm
 EOS
+}
+
+echo "----------------Desktop setup-----------------"
+echo "What desktop environment do you want?"
+echo -ne "
+1. Gnome
+2. KDE Plasma
+3. None
+"
+read de
+case $de in
+  [1]* )
+    echo "Installing Gnome"
+    install_gnome
+    ;;
+  [2]* )
+    echo "Installing Plasma"
+    install_kde
+    ;;
+  [3]*
+    echo "Not installing desktop environment"
+    ;;
+esac
 
 echo "Desktop setup done!"
